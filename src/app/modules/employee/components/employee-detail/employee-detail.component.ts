7fa82af46d50b6c16d2da2ef5b3dfb76188662db
Abdul Qayyum <abdulqayyum3810@gmail.com>
2022-08-15 20:53:45 +0500

    second commit with next and previous button

--- src/app/modules/employee/components/employee-detail/employee-detail.component.ts
+++ src/app/modules/employee/components/employee-detail/employee-detail.component.ts
@@ -7,17 +7,36 @@ import { ManageEmployeeService } from '../../services/manage-employee.service';
   styleUrls: ['./employee-detail.component.css']
 })
 export class EmployeeDetailComponent implements OnInit {
+
 @Input() emp!:Employee;
 @Input() available!:boolean;
+disabled:boolean=false;
 empList:Employee[]=this.manageEmployeeService.getEmployees()
-empDetails!:Employee;
   constructor(private manageEmployeeService:ManageEmployeeService) {
 
    }
 
   ngOnInit(): void {
   }
+  previousEmp(){
+   let a= this.empList.indexOf(this.emp);
+    if(a>0){
+      if(a===0){
+        this.disabled=true
+      }
+      else{
+        this.disabled=false
+      }
+      this.emp=this.empList[a-1]
+    }
+  }
+  nextEmp(){
+    let a= this.empList.indexOf(this.emp);
+    if(a>0 && a<this.empList.length-1){
+      this.emp=this.empList[a+1]
+  }
   
   
 
+}
 }
--- src/app/modules/employee/components/employee-list/employee-list.component.ts
+++ src/app/modules/employee/components/employee-list/employee-list.component.ts
@@ -10,7 +10,6 @@ export class EmployeeListComponent implements OnInit {
   @Output() selectedEmp= new EventEmitter<Employee>();
   empList!:Employee[];
   constructor(private manageEmployeeService:ManageEmployeeService) { 
-    this.selectedEmp.emit(new Employee())
   }
   selectEmp(emp:Employee){
     this.selectedEmp.emit(emp)
